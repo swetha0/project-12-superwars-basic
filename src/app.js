f7ae7297f73d84cc0616862ebd46a6266f8d190c
swetha0 <gaali.swetha@gmail.com>
2020-04-29 14:47:18 -0400

    ProGrad

--- src/app.js
+++ src/app.js
@@ -26,15 +26,39 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+        for(let i = 0; i < (players.length); i++)
+        {
+            if(i%2==0)
+            detailedPlayers[i] =
+            {
+                name: players[i],
+                strength: 100,
+                image:"./images/super-"+(i+1)+".png",
+                type:"hero"
+            }
+            else
+            detailedPlayers[i] =
+            {
+                name: players[i],
+                strength: 100,
+                image: "./images/super-"+(i+1)+".png",
+                type:"villian"
+            }
+        }
     return detailedPlayers;
 }
+       
 
 // getting random strength
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
+    return Math.random(1,100);
+    function randomNumber(min, max) {  
+        return Math.floor(Math.random() * (max - min) + min); 
+    }
 }
+// Return a random integer (0,100]
+    // Note: You can use Math.random() and Math.ceil()
+
 
 const buildPlayers = (players, type) => {
     let fragment = '';
@@ -42,7 +66,14 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+    for (let i = 0; i < players.length; i++) {
+        fragment = 
+        `<div class = "player">
+    <img src="${players[i].image}" alt="">
+    <div class="name">${players[i].name}</div>
+    <div class="strength">${players[i].strength}</div>
+</div>`
+    }
     return fragment;
 }
 // Display players in HTML
